ff669504b7b9b4c07aedbcbed8569fe667340920
Surbhi Goel <surbhigoel02@gmail.com>
2023-07-21 23:13:24 +0530

    completed get data function / JSON parsing

--- PostCards/PostCards/Services/DataService.swift
+++ PostCards/PostCards/Services/DataService.swift
@@ -11,6 +11,35 @@ class DataService {
     
     static func getData() -> [card]
     {
+        let pathString = Bundle.main.path(forResource: "postcard", ofType: "json")
         
+        guard pathString != nil else {
+            return [card]()
+        }
+        
+        let url = URL(fileURLWithPath: pathString!)
+        
+        do{
+            let data = try Data(contentsOf: url)
+            
+            let decoder = JSONDecoder()
+            
+            do{
+                let cardData = try decoder.decode([card].self, from: data)
+                
+                for c in cardData {
+                    c.id = UUID()
+                }
+                
+                return cardData
+            }
+            catch{
+                print(error)
+            }
+        }
+        catch{
+            print(error)
+        }
+        return [card]()
     }
 }
