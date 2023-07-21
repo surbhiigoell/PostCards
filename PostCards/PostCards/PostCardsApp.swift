7cb8bf9850e67f8cfcc4d1ac1d4dcfde73959851
Surbhi Goel <surbhigoel02@gmail.com>
2023-07-21 22:53:57 +0530

    create postcard app

---
+++ PostCards/PostCards/PostCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostCardsApp.swift
+//  PostCards
+//
+//  Created by Surbhi Goel on 21/07/23.
+//
+
+import SwiftUI
+
+@main
+struct PostCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
