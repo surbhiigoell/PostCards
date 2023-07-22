13a554dd247c9e74775161433f911077e27811c3
Surbhi Goel <surbhigoel02@gmail.com>
2023-07-22 15:43:05 +0530

    update class card

--- PostCards/PostCards/Model/cards.swift
+++ PostCards/PostCards/Model/cards.swift
@@ -12,4 +12,16 @@ class card: Identifiable , Decodable{
     var id:UUID?
     var name:String
     var image:String
+    var postcard:String
+    var date:String
+    var to:String
+    var from:String
+    var introduction:String
+    var description:String
+    var conclusion:String
+    var special_memory:String
+    var uniqueness:String
+    var future_explorations:String
+    var ideas_inspirations:[String]
+    var meaning:String
 }
